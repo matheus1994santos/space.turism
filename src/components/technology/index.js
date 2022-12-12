3be485eb56b953bf4f850393746c6ce4a52c9c6e
Matheus Noronha <matheus-nsantos@live.com>
2022-12-12 16:08:25 -0300

    Conclusão tela desk technology

--- src/components/technology/index.js
+++ src/components/technology/index.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import { Container, TitleTech } from './styled'
+import { ButtonSlide, Container, Content, NameTech, StyledStep, TechText, TitleTech } from './styled'
+import Landscape from '../../assets/technology/image-launch-vehicle-portrait.jpg'
 
-export const Technology = ({data}) => {
-  const Tech = data.technology.map( props => props );
+export const Technology = ({data = {}}) => {
+  const tech = data.technology;
+  const currentStep = 1;
 
-  console.log(Tech)
+  console.log(tech)
 
   return (
     <Container>
       <section>
         <TitleTech>
-          <p>03</p> SPACE LAUNCH 101
+          <p>03</p><span>SPACE LAUNCH 101</span>
         </TitleTech>
+        <Content>
+          <TechText>
+              <ButtonSlide>
+                { tech && tech.map( (props, index) => <StyledStep isActive={currentStep === index + 1}>{index + 1}</StyledStep> ) }
+              </ButtonSlide>
+              {tech && tech.map( ({name, description, index}) => (
+                name === 'Launch vehicle' ?
+              <div>
+                <span>THE TERMINOOGY...</span>
+                <NameTech>{name.toUpperCase()}</NameTech>
+                <p>{description}</p>
+              </div>
+              : null
+              ) )}
+          </TechText>
 
+          <img src={Landscape} />
+        </Content>
       </section>
     </Container>
   )
--- src/components/technology/styled.js
+++ src/components/technology/styled.js
@@ -2,17 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     padding-left:10.6%;
-    border: 1px solid red;
+    /* border: 1px solid red; */
     height: auto;
 
     & section{
       display: flex;
-      border: 1px solid white;
+      flex-direction: column;
+      /* border: 1px solid white; */
       height: 100%;
+      gap: 40px;
     }
 `;
 
 export const TitleTech = styled.div`
+    display: flex;
+    align-items: center;
     font-family: 'Barlow Condensed';
     font-style: normal;
     font-weight: 400;
@@ -22,11 +26,12 @@ export const TitleTech = styled.div`
     color: #FFFFFF;
 
     & p{
+      margin-right: 10px;
       font-family: 'Barlow Condensed';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 28px;
-    line-height: 34px;
+      font-style: normal;
+      font-weight: 700;
+      font-size: 28px;
+      line-height: 34px;
     /* identical to box height */
 
     letter-spacing: 4.725px;
@@ -36,4 +41,88 @@ export const TitleTech = styled.div`
     mix-blend-mode: normal;
     opacity: 0.25;
     }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: space-between;
+    /* border: 1px solid blue; */
+
+    & img{
+      width: 515px;
+      height: 527px;
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+`;
+
+export const TechText = styled.div`
+    display: flex;
+    gap: 80px;
+    padding-top: 100px;
+    width: 100%;
+
+    & div{
+      display: flex;
+      flex-direction: column;
+      height: 303px;
+
+      & span{
+        display: flex;
+        padding: 0;
+        margin: 0;
+        font-family: 'Barlow Condensed';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 19px;
+        letter-spacing: 2.7px;
+        color: #D0D6F9;         
+      }
+
+      & p{
+        font-family: 'Barlow';
+        font-style: normal;
+        width: 444px;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 32px;
+        color: #D0D6F9;        
+      }
+    }
+`;
+
+export const ButtonSlide = styled.div`
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+    /* border: 1px solid tomato; */
+    gap: 40px;
+    `;
+
+export const StyledStep = styled.button`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 70px;
+      height: 70px;
+      font-family: 'Bellefair';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 32px;
+      color: ${ props => props.isActive ? 'black': 'white' };
+      background-color: ${ props => props.isActive ? 'white': 'transparent' };
+      border-radius:80px;
+      border: 0.5px solid ${ props => !props.isActive ? 'rgb(255, 255, 255, 0.2)': 'transparent' };
+      cursor: pointer;
+      `;
+export const NameTech = styled.div`
+    font-family: 'Bellefair';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 56px;
+    line-height: 64px;
+    color: #FFFFFF;
+    padding: 0;
+    margin-top: 10px;
 `;
